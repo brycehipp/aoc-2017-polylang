3d46b77cf067a2dd3748d6012048399042a451e9
Bryce Hipp <bhipp@qetechnology.com>
2018-10-26 11:06:53 -0500

    Use variable over function

--- js/day-2/problem1.js
+++ js/day-2/problem1.js
@@ -1,13 +1,10 @@
 var fs = require('fs')
 
 const spreadsheet = fs.readFileSync('./input', 'utf8')
+const checksum = spreadsheet
+  .split(/\r\n|\r|\n/g)
+  .map(row => row.split(/\s+/).map(Number))
+  .map(row => Math.max(...row) - Math.min(...row))
+  .reduce((a, b) => a + b)
 
-function getChecksum() {
-  return spreadsheet
-    .split(/\r\n|\r|\n/g)
-    .map(row => row.split(/\s+/).map(Number))
-    .map(row => Math.max(...row) - Math.min(...row))
-    .reduce((a, b) => a + b)
-}
-
-console.log(`Checksum: ${getChecksum()}`)
+console.log(`Checksum: ${checksum}`)
